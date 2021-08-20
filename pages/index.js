717a7c2e0789ed7367ee526b921113f58c586d30
RdotSilva <RyandotSilva@gmail.com>
2021-08-20 14:27:28 -0400

    Add display for blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import matter from "gray-matter";
 
 export default function HomePage({ blogPosts }) {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {blogPosts.map((post, index) => (
+          <h3>{post.frontmatter.title}</h3>
+        ))}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
