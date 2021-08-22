aa12498a001f91714a0b11e793f66cb6d5df2dec
RdotSilva <RyandotSilva@gmail.com>
2021-08-22 13:05:59 -0400

    Add getStaticPaths function

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,3 +4,21 @@ import path from "path";
 export default function PostPage() {
   return <div>Post Page</div>;
 }
+
+/**
+ * Set the path based on slug in markdown file
+ */
+export async function getStaticPaths() {
+  const markdownFiles = fs.readdirSync(path.join("posts"));
+
+  const paths = markdownFiles.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
