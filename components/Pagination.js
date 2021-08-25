1e774fd3c895e947a3876036d5854f4cafbee643
RdotSilva <RyandotSilva@gmail.com>
2021-08-25 13:01:34 -0400

    Add display for previous link

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,3 +1,28 @@
-export default function Pagination() {
-  return <div>Pagination</div>;
+import Link from "next/link";
+
+export default function Pagination({ currentPage, numPages }) {
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === numPages;
+  const previousPageLink = `/blog/page/${currentPage - 1}`;
+  const nextPageLink = `/blog/page/${currentPage + 1}`;
+
+  // If only one page don't show any pagination
+  if (numPages === 1) {
+    return <></>;
+  }
+
+  return (
+    <div className="mt-6">
+      <ul className="flex pl-0 list-none my-2">
+        {/* Only show previous link if current page is not the first */}
+        {!isFirstPage && (
+          <Link href={previousPage}>
+            <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
+              Previous
+            </li>
+          </Link>
+        )}
+      </ul>
+    </div>
+  );
 }
