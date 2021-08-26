09b7f9e5a70f604a0b89b09fbf55e2ff627b9447
RdotSilva <RyandotSilva@gmail.com>
2021-08-26 14:03:08 -0400

    Add getStaticPaths function

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -1,5 +1,36 @@
+import fs from "fs";
+import path from "path";
 import Layout from "@/components/Layout";
+import Post from "@/components/Post";
+import CategoryList from "@/components/CategoryList";
 
-export default function CategoryBlogPage({}) {
+export default function CategoryBlogPage() {
   return <Layout>Categories</Layout>;
+}
+
+/**
+ * Get category paths from markdown files
+ */
+export async function getStaticPaths() {
+  const markdownFiles = fs.readdirSync(path.join("posts"));
+
+  const categories = markdownFiles.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return frontmatter.category.toLowerCase();
+  });
+
+  const paths = categories.map((category) => ({
+    params: { category_name: category },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
