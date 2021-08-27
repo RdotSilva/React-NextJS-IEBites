162aee2294cf9a810c7aaaf245bd3fc1c96a56b9
RdotSilva <RyandotSilva@gmail.com>
2021-08-27 13:04:15 -0400

    Clean up file

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import fs from "fs";
-import path from "path";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import matter from "gray-matter";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
+import { getPosts } from "@/lib/posts";
 
 export default function HomePage({ blogPosts }) {
   return (
@@ -28,25 +25,7 @@ export default function HomePage({ blogPosts }) {
  * Parse markdown files into blog posts objects and adds blog posts to props
  */
 export async function getStaticProps() {
-  const markdownFiles = fs.readdirSync(path.join("posts"));
-
-  const blogPosts = markdownFiles.map((filename) => {
-    const slug = filename.replace(".md", "");
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    // Parse front-matter
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
   return {
-    props: { blogPosts: blogPosts.sort(sortByDate).slice(0, 6) },
+    props: { blogPosts: getPosts().slice(0, 6) },
   };
 }
