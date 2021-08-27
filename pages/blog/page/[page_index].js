1702d605577ab69fa24223b0c076271b972e709f
RdotSilva <RyandotSilva@gmail.com>
2021-08-27 13:06:04 -0400

    Refactor getStaticProps function

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,11 +1,10 @@
 import fs from "fs";
 import path from "path";
 import Layout from "@/components/Layout";
-import matter from "gray-matter";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
 import { POSTS_PER_PAGE } from "@/config/index";
 import Pagination from "@/components/Pagination";
+import { getPosts } from "@/lib/posts";
 
 /**
  * Main Blog component used to show all blog posts
@@ -52,32 +51,16 @@ export async function getStaticPaths() {
  */
 export async function getStaticProps({ params }) {
   const page = parseInt((params && params.page_index) || 1);
-
   const markdownFiles = fs.readdirSync(path.join("posts"));
-
-  const blogPosts = markdownFiles.map((filename) => {
-    const slug = filename.replace(".md", "");
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    // Parse front-matter
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
+  const blogPosts = getPosts();
   const numPages = Math.ceil(markdownFiles.length / POSTS_PER_PAGE);
   const pageIndex = page - 1;
 
   // Sort the posts before sending as props
-  const orderedPosts = blogPosts
-    .sort(sortByDate)
-    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+  const orderedPosts = blogPosts.slice(
+    pageIndex * POSTS_PER_PAGE,
+    (pageIndex + 1) * POSTS_PER_PAGE
+  );
 
   return {
     props: { blogPosts: orderedPosts, numPages, currentPage: page },
