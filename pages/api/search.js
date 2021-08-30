b121ff257c283ed96168c459ebdd276ceb3091f6
RdotSilva <RyandotSilva@gmail.com>
2021-08-30 13:47:21 -0400

    Add handler to filter posts by search

---
+++ pages/api/search.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+/**
+ * Handler used to parse the markdown files and filter out posts based on search criteria
+ */
+export default function handler(req, res) {
+  let blogPosts;
+
+  if (process.env.NODE_ENV === "production") {
+    //TODO: Fetch from cache
+  } else {
+    const markdownFiles = fs.readdirSync(path.join("posts"));
+
+    blogPosts = markdownFiles.map((filename) => {
+      const markdownWithMeta = fs.readFileSync(
+        path.join("posts", filename),
+        "utf-8"
+      );
+
+      const { data: frontmatter } = matter(markdownWithMeta);
+
+      return {
+        frontmatter,
+      };
+    });
+  }
+
+  // Get the results from the request search query
+  const results = blogPosts.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1
+  );
+
+  res.status(200).json(JSON.stringify({ results }));
+}
