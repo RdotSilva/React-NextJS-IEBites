ddde77cac674d9f9685196a2df19819df5d04b24
RdotSilva <RyandotSilva@gmail.com>
2021-08-31 14:57:06 -0400

    Add postData function TODO

---
+++ scripts/cache.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const matter = require("gray-matter");
+
+function postData() {
+  // TODO: Create function that will read the markdown post data, cache it, and store in a js file
+}
