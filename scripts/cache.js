420e1bfe1ffa520220cec6d823049df2d4ea8d65
RdotSilva <RyandotSilva@gmail.com>
2021-09-01 13:09:04 -0400

    Add logic to cache post data

--- scripts/cache.js
+++ scripts/cache.js
@@ -3,5 +3,37 @@ const path = require("path");
 const matter = require("gray-matter");
 
 function postData() {
-  // TODO: Create function that will read the markdown post data, cache it, and store in a js file
+  const markdownFiles = fs.readdirSync(path.join("posts"));
+
+  const blogPosts = markdownFiles.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  // Make the blog posts available to other files
+  return `export const posts = ${JSON.stringify(blogPosts)}`;
 }
+
+// Check if we already have a cache, if not create it
+try {
+  fs.readdirSync("cache");
+} catch (error) {
+  fs.mkdirSync("cache");
+}
+
+// Fetch the post data and write to cache
+fs.writeFile("cache/data.js", postData(), function (err) {
+  if (err) return console.log(err);
+  console.log("Posts Cached...");
+});
