43444edac07cfaab74a974bd8de1098b7a3e6e3c
RdotSilva <RyandotSilva@gmail.com>
2021-09-02 14:10:17 -0400

    Add cached blog post data

---
+++ cache/data.js
@@ -0,0 +1,38 @@
+export const posts = [
+  {
+    slug: "test-post-one",
+    frontmatter: {
+      title: "Test Post 1",
+      date: "May 5, 2021",
+      excerpt: "Testing Post 1",
+      cover_image: "/images/posts/img.jpg",
+      category: "food",
+      author: "John Doe",
+      author_image: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+  },
+  {
+    slug: "test-post-three",
+    frontmatter: {
+      title: "Test Post 3",
+      date: "December 15, 2021",
+      excerpt: "Testing Post 3",
+      cover_image: "/images/posts/img.jpg",
+      category: "food",
+      author: "John Doe",
+      author_image: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+  },
+  {
+    slug: "test-post-two",
+    frontmatter: {
+      title: "Test Post 2",
+      date: "April 1, 2021",
+      excerpt: "Testing Post 2",
+      cover_image: "/images/posts/img.jpg",
+      category: "food",
+      author: "John Doe",
+      author_image: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+  },
+];
